120dcb884c783a416ffaaae5d714334f727802e1
Govnor Payne <govnorpayne@gmail.com>
2022-02-23 18:34:11 -0600

    corrected function locations  and return loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,4 @@
 // Assignment code here
-var promptlengthtext = window.prompt(
-  "Password length must be between 8-128 numbers"
-);
-if (
-  promptlengthtext === "" ||
-  promptlengthtext === null ||
-  parseInt(promptlengthtext) < 8 ||
-  parseInt(promptlengthtext) > 128 ||
-  isNaN(promptlengthtext)
-) {
-  window.alert("You need to provide a valid answer! Please try again.");
-}
-if (promptlengthtext >= 8 && promptlengthtext <= 128) {
-  var Lowercase = confirm("Do you want a lowercase letter?");
-  var Uppercase = confirm("Do you want a uppercase letter?");
-  var Number = confirm("Do you want a number?");
-  var Symbol = confirm("Do you want a symbol?");
-}
-while (Lowercase != true && Uppercase != true && Number != true && Symbol) {
-  window.prompt("Must pick at least one of the values to generate a password!");
-  Lowercase = confirm("Do you want a lowercase letter?");
-  Uppercase = confirm("Do you want a uppercase letter?");
-  Number = confirm("Do you want a number?");
-  Symbol = confirm("Do you want a symbol?");
-}
 
 const randomFunc = {
   lower: getRandomLower,
@@ -37,40 +12,74 @@ var generateEl = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = generatePassword();
 }
 
 // Add event listener to generate button
 generateEl.addEventListener("click", (event) => {
-  const hasLower = Lowercase.true;
-  const hasUpper = Uppercase.true;
-  const hasNumber = Number.true;
-  const hasSymbol = Symbol.true;
-  resultEl.innerText = generatePassword(
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol,
-    length
-  );
+  writePassword();
 });
 
-function generatePassword(lower, upper, number, symbol, length) {
-  let generatePassword = "";
-  const typesCounted = lower + upper + number + symbol;
+function generatePassword() {
+  let Password = "";
+  var Lowercase = false;
+  var Uppercase = false;
+  var Number = false;
+  var Symbol = false;
+
+  var promptlengthtext = window.prompt(
+    "Password length must be between 8-128 numbers"
+  );
+  if (
+    promptlengthtext === "" ||
+    promptlengthtext === null ||
+    parseInt(promptlengthtext) < 8 ||
+    parseInt(promptlengthtext) > 128 ||
+    isNaN(promptlengthtext)
+  ) {
+    alert("You need to provide a valid answer! Please try again.");
+    generatePassword();
+  }
+  if (parseInt(promptlengthtext) >= 8 && parseInt(promptlengthtext) <= 128) {
+    Lowercase = confirm("Do you want a lowercase letter?");
+    Uppercase = confirm("Do you want a uppercase letter?");
+    Number = confirm("Do you want a number?");
+    Symbol = confirm("Do you want a symbol?");
+  }
+  while (
+    Lowercase != true &&
+    Uppercase != true &&
+    Number != true &&
+    Symbol != true
+  ) {
+    console.log(Lowercase, Uppercase, Number, Symbol);
+    alert("Must pick at least one of the values to generate a password!");
+    Lowercase = confirm("Do you want a lowercase letter?");
+    Uppercase = confirm("Do you want a uppercase letter?");
+    Number = confirm("Do you want a number?");
+    Symbol = confirm("Do you want a symbol?");
+  }
+
   const typesArr = [
     {
-      lower,
+      lower: Lowercase,
     },
-    { upper },
-    { symbol },
+    { upper: Uppercase },
+    { symbol: Symbol },
     {
-      number,
+      number: Number,
     },
   ].filter((item) => Object.values(item)[0]);
+
+  for (let i = 0; i < promptlengthtext; i++) {
+    var randomIndex = Math.floor(Math.random() * typesArr.length);
+    const funcName = Object.keys(typesArr[randomIndex]);
+    Password += randomFunc[funcName]();
+  }
+  console.log(Password);
+  return Password;
 }
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
